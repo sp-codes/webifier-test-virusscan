f915f253f30159df7865b47dd2d46128112e2ffb
samuel-p <samuel.philipp@t-online.de>
2017-02-20 16:19:24 +0100

    test optimized

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 # $1 = URL
 # $2 = tmp dir
 
-httrack --mirrorlinks --keep-links=K --do-not-log --sockets=8 --robots=1 --retries=1 --depth=2 $1 -O $2
+httrack --get-files --keep-links=K --do-not-log --sockets=8 --robots=0 --retries=2 --depth=9999 $1 -O $2
 cd $2
 rm -rf hts-cache
 find -maxdepth 1 -type f -delete
